d63e991f20b09684ea5a074380451c4760b48d9d
ThomasSchuppler <thomas.schuppler@bulme.at>
2025-11-20 17:38:22 +0100

    Update main.cpp

--- PlatformioSchuppler/arduino-simon-game-main/arduino-simon-game-main/src/main.cpp
+++ PlatformioSchuppler/arduino-simon-game-main/arduino-simon-game-main/src/main.cpp
@@ -1,11 +1,44 @@
 #include <Arduino.h>
 
+int counter = 0;
+
 void setup()
 {
   Serial.begin(9600);
 }
 
 void loop(){
-  Serial.print("ABC");
+  counter++;
+  if (counter % 3 == 0)
+  {
+    if (counter % 5 ==0)
+    {
+      Serial.println("FIZZ BUZZ");
+      /* code */
+    }
+    else
+    {
+      Serial.println("BUZZ");
+    }
+    
+    /* code */
+  }
+  else 
+  {
+    if (counter % 5 ==0)
+    {
+      Serial.println("Buzz");
+      /* code */
+    }
+    else{
+      Serial.println(counter);
+    }
+  }
+
+  
+  
+  
+
+
   delay(500);
 }
