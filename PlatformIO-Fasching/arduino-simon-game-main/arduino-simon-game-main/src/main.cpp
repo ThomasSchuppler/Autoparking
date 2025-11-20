50138df944dcc09a51b05d121e1f72814da1f7a9
julian-fasching06 <julian.fasching@bulme.at>
2025-11-20 17:38:05 +0100

    FizzBuzz simulator

--- PlatformIO-Fasching/arduino-simon-game-main/arduino-simon-game-main/src/main.cpp
+++ PlatformIO-Fasching/arduino-simon-game-main/arduino-simon-game-main/src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+int counter = 0;
+
 void setup() {
   Serial.begin(9600);
 }
@@ -7,7 +9,43 @@ void setup() {
 /* Digit table for the 7-segment display */
 
 void loop() {
-  Serial.print("ABC");
+  counter ++;
+
+  if (counter % 3 == 0)
+  {
+    if (counter % 5 == 0)
+    {
+      Serial.print("Fizz Buzz");
+    }
+    else
+    {
+      Serial.print("Fizz ");
+      
+    }
+    
+    
+    
+  }
+  else
+  {
+    if (counter % 5 == 0)
+    {
+      Serial.print(" Buzz");
+      
+    }
+    else
+    {
+      Serial.println("counter");
+      
+    }
+    
+    
+  }
+  
+  
+
+
+
   delay(500);
   
 }
